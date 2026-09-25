b9be0c4570691f4e4bdbff17a6bbde986822002b
MediaTeck <rangga.4alone@gmail.com>
2026-09-25 10:12:01 +0700

    feat: export trigger validation API

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./domain/deck.js";
 export * from "./engine/program-engine.js";
 export * from "./engine/trigger-engine.js";
 export * from "./engine/output-engine.js";
+export * from "./engine/trigger-validator.js";
